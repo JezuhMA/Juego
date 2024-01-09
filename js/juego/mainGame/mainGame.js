f8cdee85fe5e42225e35cf007a2baedceff0e48b
JezuhMA <114490021+JezuhMA@users.noreply.github.com>
2024-01-09 12:46:02 +0100

    animada la serpiente

--- js/juego/mainGame/mainGame.js
+++ js/juego/mainGame/mainGame.js
@@ -1,72 +1,89 @@
-import Snake from './snake.js';
+import Snake from "./snake.js";
 
-const TAMANO_TABLERO = 20;
+const TAMANO_TABLERO = 40;
+
+const movimiento = { x: 0, y: -1 };
 
 const serpi = new Snake(TAMANO_TABLERO);
 
 let intervalId;
 
 //Movimiento de la serpiente
-document.addEventListener('keydown', function(event) {
-    if (event.key === 'ArrowLeft') {
-        serpi.moverSnake(-1, 0);
-    }
-    if (event.key === 'ArrowUp') {
-        serpi.moverSnake(0, -1);
-    }
-    if (event.key === 'ArrowDown') {
-        serpi.moverSnake(0, 1);
-    }
-    if(event.key === "ArrowRight"){
-        serpi.moverSnake(1, 0);
-    }
-
-    if(!serpi.limitarSnake()) dibujarJuego();
+document.addEventListener("keydown", function (event) {
+	movimiento.x = 0;
+	movimiento.y = 0;
+	if (event.key === "ArrowLeft") {
+		movimiento.x = -1;
+		movimiento.y = 0;
+	}
+	if (event.key === "ArrowUp") {
+		movimiento.x = 0;
+		movimiento.y = -1;
+	}
+	if (event.key === "ArrowDown") {
+		movimiento.x = 0;
+		movimiento.y = 1;
+	}
+	if (event.key === "ArrowRight") {
+		movimiento.x = 1;
+		movimiento.y = 0;
+	}
 });
 
-function dibujarJuego(){
-    const tablero = document.getElementById("tablero");
-    const frag = document.createDocumentFragment();
+function dibujarJuego() {
+	const tablero = document.getElementById("tablero");
+	const frag = document.createDocumentFragment();
 
-    //Limpiamos si hubiera algo
-    while (tablero.firstChild) {
-        tablero.firstChild.remove();
-    }
+	//Limpiamos si hubiera algo
+	while (tablero.firstChild) {
+		tablero.firstChild.remove();
+	}
 
-    for (let x = 0; x < TAMANO_TABLERO; x++) {
-        for (let y = 0; y < TAMANO_TABLERO; y++) {
-            const pixel = document.createElement('div');
-            
-            pixel.classList.add('pixel');
+	for (let y = 0; y < TAMANO_TABLERO; y++) {
+		for (let x = 0; x < TAMANO_TABLERO; x++) {
+			const pixel = document.createElement("div");
 
-            pixel.style.left = `${20 * x}px`;
-            pixel.style.top = `${20 * y}px`;
+			pixel.classList.add("pixel");
 
-            if (serpi.cuerpo.some(segemento => segemento.posicion.x === x && segemento.posicion.y === y)) {
-                //const img = document.createElement('img');
-                //img.src()
-                //pixel.appendChild();
-                pixel.style.background = 'green';
-            } else {//if(manzana.x === x && manzana.y === y){
-                //pixel.style.background = 'red';
-            }
-            frag.appendChild(pixel);
-        }
-        
-    }
-    tablero.appendChild(frag);
-    
+			pixel.style.left = `${20 * x}px`;
+			pixel.style.top = `${20 * y}px`;
+
+			if (
+				serpi.cuerpo.some(
+					(segemento) =>
+						segemento.posicion.x === x && segemento.posicion.y === y
+				)
+			) {
+				//const img = document.createElement('img');
+				//img.src()
+				//pixel.appendChild();
+				pixel.style.background = "green";
+			} else {
+				//if(manzana.x === x && manzana.y === y){
+				//pixel.style.background = 'red';
+			}
+			frag.appendChild(pixel);
+		}
+	}
+	tablero.appendChild(frag);
+}
+function animacionSerpiente() {
+	if (!serpi.limitarSnake(movimiento.x, movimiento.y)) {
+		serpi.moverSnake(movimiento.x, movimiento.y);
+		return true;
+	}
+	return false;
 }
 
-//Animacion de la pantalla
-const iniciarAnimacion = () => {
-    
-    intervalId = requestAnimationFrame(iniciarAnimacion);
+function animar() {
+	if (animacionSerpiente()) {
+		dibujarJuego();
+	}
+
+	requestAnimationFrame(animar);
 }
 
 window.onload = () => {
-    
-    dibujarJuego();
-    
-    intervalId = requestAnimationFrame(iniciarAnimacion);
-}
+	dibujarJuego();
+	intervalId = requestAnimationFrame(animar);
+};
--- js/juego/mainGame/snake.js
+++ js/juego/mainGame/snake.js
@@ -9,18 +9,23 @@ export default function Snake(TAMANO_TABLERO) {
         cuerpo : [cabeza,abdomen],
         //TODO modificar esta funcion para Mejorar el movimiento
         moverSnake : function moverSnake (movX , movY) {
-            this.cuerpo.forEach(segmento => {
-                segmento.posicion.x += movX;
-                segmento.posicion.y += movY;
-            });
+            const nuevaParte = this.cuerpo[this.cuerpo.length - 1];
+            //Movimiento del resto del cuerpo
+            for (let i = 1; i < this.cuerpo.length; i++) {
+                this.cuerpo[i].posicion.x = this.cuerpo[i - 1].posicion.x;
+                this.cuerpo[i].posicion.y = this.cuerpo[i - 1].posicion.y;
+                
+            }
+            //Movimiento de la cabeza
+            this.cuerpo[0].posicion.x += movX;
+            this.cuerpo[0].posicion.y += movY;
         },
 
         //limitarSnake devolverá true si alguna parte de la serpiente está fuera del tablero, y false si toda la serpiente está dentro del tablero.
-        limitarSnake : function limitar () {
-            return this.cuerpo.some(segmento => {
-                return (segmento.posicion.x < 0 ||  segmento.posicion.x > TAMANO_TABLERO ||
-                        segmento.posicion.y < 0 || segmento.posicion.y > TAMANO_TABLERO);
-            });
+        limitarSnake : function limitar (movX , movY) {
+                const segmento = this.cuerpo[0];
+                return (segmento.posicion.x + movX < 0 ||  segmento.posicion.x + movX > TAMANO_TABLERO - 1 ||
+                        segmento.posicion.y + movY < 0 || segmento.posicion.y + movY > TAMANO_TABLERO - 1);
         }
 
     }
