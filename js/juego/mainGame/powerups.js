e19eb284da557e3a619719d80f216d3d149c58ba
Yisas <1686231@alu.murciaeduca.es>
2024-01-11 13:39:25 +0100

    Poweerups mejores

--- js/juego/mainGame/powerups.js
+++ js/juego/mainGame/powerups.js
@@ -2,12 +2,12 @@
 export default function PowerUps(id, posiciones) {
 
     const seguimiento = new Map();
-    const idSeguimiento = 0;
+    let idSeguimiento = 0;
 
     function posicionar() {
         const posicionamiento = { x: null, y: null };
         for (const prop of posicionamiento) {
-            if (Object.hasOwnProperty.call(iterator, posicionamiento)) {
+            if (Object.hasOwnProperty.call(prop, posicionamiento)) {
                 for (const coorde of posiciones) {
                     posicionamiento[prop] = coorde;
                 }
@@ -24,73 +24,53 @@ export default function PowerUps(id, posiciones) {
                     status: 'OK',
                     img: 'imagenes/juego/powerups/apple',
                     alt: 'imagen no encontrada',
-                }
-                break;
+                };
             case 'PosinedApple':
 
                 return {
                     status: 'OK',
                     img: 'imagenes/juego/powerups/poisonedApple',
                     alt: 'imagen no encontrada',
-                }
-                break;
+                };
             case 'Star':
                 return {
                     status: 'OK',
                     img: 'imagenes/juego/powerups/star',
                     alt: 'imagen no encontrada',
-                }
-                break;
+                };
             case 'Portal':
                 return {
                     status: 'OK',
-                    img: 'imagenes/juego/powerups/portalEntrada',
-                    img: 'imagenes/juego/powerups/portalSalida',
+                    imgEntrada: 'imagenes/juego/powerups/portalEntrada',
+                    imgSalida: 'imagenes/juego/powerups/portalSalida',
                     alt: 'imagen no encontrada',
-                }
-                break;
-
-
+                };
             default:
                 return {
                     status: 'Not found',
                     error: 'parametro equivocado a la hora de introducir el id del powerup',
-                }
-                break;
+                };
         }
     }
 
     function addPowerUP(id) {
+
+        const powerUpObj = {
+            id: id,
+            posicion: posicionar(),
+            img: establecerImagen(id),
+        };
+
         switch (id) {
             case 'Apple':
-                seguimiento.set(idSeguimiento, id);
-                idSeguimiento++;
-                return true;
-
-                break;
             case 'PosinedApple':
-
-                seguimiento.set(idSeguimiento, id);
-                idSeguimiento++;
-                return true;
-
-                break;
             case 'Star':
-                seguimiento.set(idSeguimiento, id);
-                idSeguimiento++;
-                return true;
-                break;
             case 'Portal':
-
-                seguimiento.set(idSeguimiento, id);
+                seguimiento.set(idSeguimiento, powerUpObj);
                 idSeguimiento++;
                 return true;
-                break;
-
-
             default:
                 return false;
-                break;
         }
     }
 
--- js/juego/mainGame/snake.js
+++ js/juego/mainGame/snake.js
@@ -1,22 +1,22 @@
 export default function Snake(TAMANO_TABLERO) {
-    
-    let cabeza = { img: "imagenes/serpiente/", posicion : {x: TAMANO_TABLERO / 2, y: TAMANO_TABLERO / 2}};
-    let abdomen = { img: "imagenes/serpiente/", posicion : {x: TAMANO_TABLERO / 2, y: TAMANO_TABLERO / 2 - 1}};
 
-    return  {
+    let cabeza = { img: "imagenes/serpiente/", posicion: { x: TAMANO_TABLERO / 2, y: TAMANO_TABLERO / 2 } };
+    let abdomen = { img: "imagenes/serpiente/", posicion: { x: TAMANO_TABLERO / 2, y: TAMANO_TABLERO / 2 - 1 } };
+    //TODO: Hacer el cuerpo dinamico y cambiar la posicion de origen segun el tablero
+    return {
         cabeza: cabeza,
         abdomen: abdomen,
-        cuerpo : [cabeza,abdomen],
-        //TODO modificar esta funcion para Mejorar el movimiento
+        cuerpo: [cabeza, abdomen],
+
         moverSnake: function moverSnake(movX, movY) {
             // Crear una copia del último segmento de la serpiente
             let nuevoSegmento = Object.assign({}, this.cuerpo[this.cuerpo.length - 1]);
-        
+
             // Mover cada segmento de la serpiente a la posición del segmento anterior
             for (let i = this.cuerpo.length - 1; i > 0; i--) {
                 this.cuerpo[i].posicion = Object.assign({}, this.cuerpo[i - 1].posicion);
             }
-        
+
             // Mover la cabeza de la serpiente
             this.cuerpo[0].posicion.x += movX;
             this.cuerpo[0].posicion.y += movY;
@@ -25,10 +25,10 @@ export default function Snake(TAMANO_TABLERO) {
         },
 
         //limitarSnake devolverá true si alguna parte de la serpiente está fuera del tablero, y false si toda la serpiente está dentro del tablero.
-        limitarSnake : function limitar (movX , movY) {
-                const segmento = this.cuerpo[0];
-                return (segmento.posicion.x + movX < 0 ||  segmento.posicion.x + movX > TAMANO_TABLERO - 1 ||
-                        segmento.posicion.y + movY < 0 || segmento.posicion.y + movY > TAMANO_TABLERO - 1);
+        limitarSnake: function limitar(movX, movY) {
+            const segmento = this.cuerpo[0];
+            return (segmento.posicion.x + movX < 0 || segmento.posicion.x + movX > TAMANO_TABLERO - 1 ||
+                segmento.posicion.y + movY < 0 || segmento.posicion.y + movY > TAMANO_TABLERO - 1);
         }
 
     }
