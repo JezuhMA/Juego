f5dbf6249c8be5945d9365d4ce294eab52f0338e
JezuhMA <114490021+JezuhMA@users.noreply.github.com>
2024-02-02 17:23:18 +0100

    multiples cambios a las funciones del juego añadidos datasets a los pixeles para controlar el movimiento creadas funciones para iniciar el juego y para mover la serpiente mediante eventos de pulsar teclas

--- src/main/webapp/js/juego/mainGame/mainGame.js
+++ src/main/webapp/js/juego/mainGame/mainGame.js
@@ -8,7 +8,6 @@ window.onload = ()=> {
     // CUERPOSERPIENTE = 2
     // MANZANA = 3
     // FONDO = 0
-    let estado = - 1;
 
     // AQUI RELLENO EL ARRAY_JUEGO PARA QUE TENGA EL TAMAÑO DEL TABLERO
  	for (let i = 0; i < TABLERO_TAM; i++) {
@@ -18,27 +17,32 @@ window.onload = ()=> {
 		}
 	}
 
-    array_Juego[38][20] = 1; //Pinto cabeza
-    array_Juego[39][20] = 1;
+    array_Juego[39][20] = 1; //Pinto cabeza
     array_Juego[20][15] = 3; //Pinto manzana
 
-    function animacionJuego() {
+    //ESTA FUNCION INICIA EL BUCLE
+    function inicio() {
         // AQUI RECORRO EL ARRAY Y PINTO LOS PIXELES
         for (let x = 0; x < TABLERO_TAM; x++) {
             for (let y = 0; y < TABLERO_TAM; y++) {
                 const pixel = document.createElement("div");
+                pixel.dataset.x = `${x}`;
+                pixel.dataset.y = `${y}`;
                 pixel.style.left = `${x * pixel_tam}px`;
                 pixel.style.top = `${y * pixel_tam}px`;
                 pixel.style.width = `${pixel_tam}px`;
                 pixel.style.height = `${pixel_tam}px`;
                 pixel.style.position = "absolute";
-                estado = array_Juego[x][y];
+                const estado = array_Juego[x][y];
                 if (estado === 0) {
                     pixel.style.backgroundColor = "black";
-                } else if (estado === 1 || estado === 2) { //PINTO LA CABEZA
+                    pixel.dataset.status = `${estado}`;
+                } else if (estado === 1 || estado === 2) { //PINTO LA CABEZA Y EL CUERPO
                     pixel.style.backgroundColor = "green";
+                    pixel.dataset.status = `${estado}`;
                 } else if (estado === 3) {
-                    pixel.style.backgroundColor = "red";
+                    pixel.style.backgroundColor = "red"
+                    pixel.dataset.status = `${estado}`;
                 }
                 frag.appendChild(pixel);
             }
@@ -47,37 +51,106 @@ window.onload = ()=> {
         tablero.appendChild(frag);
     }
 
-    animacionJuego();
+    inicio();
+
+    function animacionJuego(){
+        const pixeles = document.querySelectorAll(`[data-status]`);
+        //SI YO ME QUIERO MOVER A LA DERECHA SUMO UNO A LA X & MANTENGO LA Y
+        pixeles.forEach(pixel =>{
+            let estado = parseInt(pixel.dataset.status); //ESTADO ANTES DE HACER CAMBIOS
+            let posX = parseInt(pixel.dataset.x);
+            let posY = parseInt(pixel.dataset.y);
+            let estadoArr = array_Juego[posX][posY];
+            if (estado !== estadoArr){
+                pixel.dataset.status = `${estadoArr}`;
+                switch (estadoArr){
+                    case 0 :{
+                        pixel.style.backgroundColor = "black";
+                        break;
+                    }
+                    case 1 :
+                    case 2 : {
+                        pixel.style.backgroundColor = "green";
+                        break;
+                    }
+                    case 3 : {
+                        pixel.style.backgroundColor = "red";
+                    }
+                }
+            }
+        })
+
+    }
 
     window.addEventListener("keydown", (event)=>{
         event.preventDefault();
         switch (event.code) {
             case "ArrowLeft" : {
+                let pararBucles = false; //variable de control para parar el bucle externo
                 for (let x = 0; x < TABLERO_TAM; x++) {
+                    if (pararBucles) break;
                     for (let y = 0; y < TABLERO_TAM; y++) {
-                        estado = array_Juego[x][y];
+                        let estado = array_Juego[x][y];
                         if (estado === 1){
-                            array_Juego[ x - 1 ][y] = estado;
+                            array_Juego[ x - 1 ][y] = 1;
                             array_Juego[x][y] = 0;
-                            animacionJuego();
-                            estado = -1;
+                            pararBucles = true;
+                            break;
                         }
                     }
                 }
+                animacionJuego();
+                break;
+            }
+            case "ArrowRight" : {
+                let pararBucles = false;
+                for (let x = 0; x < TABLERO_TAM; x++) {
+                    if (pararBucles) break;
+                    for (let y = 0; y < TABLERO_TAM; y++) {
+                        let estado = array_Juego[x][y];
+                        if (estado === 1){
+                            array_Juego[ x + 1 ][y] = 1;
+                            array_Juego[x][y] = 0;
+                            pararBucles = true;
+                            break;
+                        }
+                    }
+
+                }
+                animacionJuego();
+                break;
+            }
+            case "ArrowDown" : {
+                let pararBucles = false;
+                for (let x = 0; x < TABLERO_TAM; x++) {
+                    if (pararBucles) break;
+                    for (let y = 0; y < TABLERO_TAM; y++) {
+                        let estado = array_Juego[x][y];
+                        if (estado === 1){
+                            array_Juego[x][y + 1] = 1;
+                            array_Juego[x][y] = 0;
+                            break;
+                        }
+                    }
+                }
+                animacionJuego();
                 break;
             }
             case "ArrowUp" : {
+                let pararBucles = false;
                 for (let x = 0; x < TABLERO_TAM; x++) {
+                    if(pararBucles) break;
                     for (let y = 0; y < TABLERO_TAM; y++) {
-                        estado = array_Juego[x][y];
+                        let estado = array_Juego[x][y];
                         if (estado === 1){
-                            array_Juego[x][y - 1] = estado;
+                            array_Juego[x][y - 1] = 1;
                             array_Juego[x][y] = 0;
-                            animacionJuego();
-                            estado = -1;
+                            pararBucles = true;
+                            break;
                         }
                     }
                 }
+                animacionJuego();
                 break;
             }
         }
